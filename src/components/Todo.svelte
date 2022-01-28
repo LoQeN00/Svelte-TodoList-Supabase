d83f1742993c142c0cfcc19b0a3c4e2b66e8a21c
LoQeN00 <LoQeN00@o2.pl>
2022-01-28 22:08:20 +0100

    Todo done

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -5,19 +5,29 @@
     export let todo:TodoType;
 
 </script>
-
-<div class="flex justify-between bg-white items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4">
-    <div class="max-w-[80%] break-words break-all">
-        <p class={`text-gray-800 ${todo.completed && 'line-through'}`}>{todo.text}</p>
-    </div>
-    <input
+    <div class="flex outline-dashedbg-white items-center shadow-sm border border-gray-200 rounded-lg my-2 py-2 px-4">
+        <input
         name="completed"
         type="checkbox"
         checked={todo.completed}
         on:change={() => toggleTodoCompleted(todo.id)}
         class="mr-2 form-checkbox h-5 w-5"
         />
-</div>
+        <div class=" break-words break-all w-[90%] p-4">
+            <p class={`text-gray-800 ${todo.completed && 'line-through'}`}>{todo.text}</p>
+        </div>
+
+        <button
+        class="text-sm bg-red-500 hover:bg-red-600 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+        on:click={() => deleteTodo(todo.id)}
+        >
+        Delete
+    </button>
+      
+    </div>
+    
+
+
 
 
 
